68bd57d92157b35525067c024b76378f68122689
Georges Racinet on ishtar.racinet.fr <georges@racinet.fr>
2012-10-23 01:40:17 +0200

    proof of concept for ajax calls

--- __init__.py
+++ __init__.py
@@ -1,1 +1,23 @@
-# python package
+import simplejson
+try:
+    import openerp.addons.web.common.http as openerpweb
+except ImportError:
+    import web.common.http as openerpweb
+
+class PocController(openerpweb.Controller):
+
+    _cp_path = '/poc'
+
+    @openerpweb.httprequest
+    def hello(self, request):
+        return 'Hello, client\n'
+
+    @openerpweb.jsonrequest
+    def whoami(self, request):
+        """Return the name of the current user."""
+        # auth has to be refreshed client side before
+        # (can be done calling needed request.session.authenticate)
+        # but there are shortcuts in json_rpc calls (see core.js)
+        # that *probably* make this more or less automatic
+        session = request.session
+        return session.model('res.users').read([session._uid])['name']
