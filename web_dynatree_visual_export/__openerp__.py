83ba10e57d95fbcfd51ce416004ad0ac7b1f3808
jssuzanne <devnull@localhost>
2013-07-29 16:24:19 +0200

    Not be a standaloen + add visual export for dynatree

---
+++ web_dynatree_visual_export/__openerp__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Web dynatree global export',
+    'version': '0.0.1',
+    'sequence': 150,
+    'category': 'Anybox',
+    'description': """
+    """,
+    'author': 'ANYBOX',
+    'website': 'www.anybox.fr',
+    'depends': [
+        'base',
+        'visual_export',
+        'web_dynatree',
+    ],
+    'js': [
+        'static/src/js/tree_dynatree.js',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
