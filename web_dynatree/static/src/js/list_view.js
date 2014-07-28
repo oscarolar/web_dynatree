e32240c3b779dbd6d02c2362514d9b1c9d4e2eaa
oalca <oszckar@gmail.com>
2014-07-28 12:41:07 -0500

    [ADD] the clean render of the wbs tree

--- web_dynatree/static/src/js/list_view.js
+++ web_dynatree/static/src/js/list_view.js
@@ -10,7 +10,46 @@ openerp.web_dynatree.tree_view = function(instance){
         view_type: 'list_dynatree',
         init: function(parent, dataset, view_id, options) {
             console.log('LIST_view');
+            this._dynatrees = {};
             this._super(parent, dataset || null, view_id || null, options || {});
+        },
+        start: function () {
+            console.log('tree_view start');
+            this.display_dynatree();
+            return this._super();
+        },
+        display_dynatree: function () {
+            var self = this;
+            if (this.options.action.dynatree_setting_ids) {
+                dynatreeconf = new instance.web.Model('ir.actions.act_window.dynatree');
+                dynatreeconf.call('get_dynatrees',
+                        [this.options.action.dynatree_setting_ids],
+                        {context: this.dataset.context}).then(function (dynatrees) {
+                    _(dynatrees).each( function (dynatree) {
+                        self._dynatrees[dynatree.id] = [];
+                        var d = QWeb.render('TreeViewDynatree.Dynatree',
+                                            {'dynatree': dynatree});
+                        $(self.$el[0]).append(d);
+                        new instance.web.Dynatree({
+                            dynatree_id: dynatree.id,
+                            configuration: {
+                                model: dynatree.model,
+                                first_node_domain: dynatree.init_domain,
+                                child_field: dynatree.child_field,
+                                checkbox_field: dynatree.checkbox_field,
+                            },
+                            context: dynatree.context,
+                            use_checkbox: true,
+                            onSelect: function(model_ids){
+                                self._dynatrees[dynatree.id] = model_ids;
+                                return self.load_view(self.dataset.context);
+                            },
+                            selectMode: dynatree.selectMode
+                        });
+                    });
+
+                });
+            }
         },
     });
 };
