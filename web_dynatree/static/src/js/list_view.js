b55f6f58ec9f0dcb3559f1280b02fb04d6d72894
oalca <oszckar@gmail.com>
2014-08-01 10:07:29 -0500

    [FIX] fixed dynatree declaration

--- web_dynatree/static/src/js/list_view.js
+++ web_dynatree/static/src/js/list_view.js
@@ -1,4 +1,4 @@
-openerp.web_dynatree.tree_view = function(instance){
+openerp.web_dynatree.list_view = function(instance){
     var _t = instance.web._t,
         _lt = instance.web._lt;
     var QWeb = instance.web.qweb;
@@ -50,6 +50,10 @@ openerp.web_dynatree.tree_view = function(instance){
 
                 });
             }
+        },
+        view_loading: function(r) {
+            console.log("LOAD_LIST");
+            return this.load_list(r);
         },
     });
 };
